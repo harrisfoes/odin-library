8446dfbb4d1ed6c6d4f97a4b589b316e4ab4734b
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2023-08-29 20:02:09 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # odin-library
 Mini library app for odin-project that is a demonstration of how Objects work in Javascript
+
+URL:
+
+## Requirements
+[ ] create javascript prototype that works on console
+    adding new objects into an array
+    
---
+++ main.js
@@ -0,0 +1,20 @@
+/*
+- write a function that loops through the array and displays each book on the page
+- display them as a table or each on their own card
+- add a NEW BOOK button that brings up a form allowing users to input the details for the new book:
+ - author, title, number of pages, whether it's been read
+ - you may want to explore the dialog tag
+- add a button on each book's display to remove the book from the library
+    - use data attribute which corresponds to the index of the library array
+- add a button on each book's display to change its read status
+*/
+
+const myLibrary = [];
+
+function Book() {
+    // the constructor...
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
